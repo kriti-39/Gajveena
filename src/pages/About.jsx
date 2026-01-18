c439d0e2d02d1d0e10cb91ba1b1bd17dc78108f7
Kriti <kritipriya030902@gmail.com>
2026-01-18 17:29:49 +0530

    done changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,9 +11,10 @@ const AboutPage = () => {
   return (
     <>
       <Navbar />
+      <div className='pt-20'></div>
       <CreatorHero/>
       <Creator/>
-      <div className="text-center pt-12">
+      <div className="text-center pt-10">
         <a
           href="http://debjitmahalanobis.com/"
           className="text-xl text-yellowTint font-bold hover:text-white transition-colors duration-300"
